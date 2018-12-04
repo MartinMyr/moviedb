92101fbcf17b0f14ca0d7b03cbaf3651f44352b5
Direktonline-Martin <martin.myrmarker@gmail.com>
2018-12-04 18:41:25 +0100

    sort by desc on rating

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import MovieRow from './MovieRow.js';
 import $ from 'jquery';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import '../node_modules/font-awesome/css/font-awesome.min.css';
 
 
 class App extends Component {
@@ -9,8 +11,11 @@ class App extends Component {
     super(props);
     this.state = {};
   }
-
+  onClick(){
+    console.log("asd");
+  }
   performSearch(searchTerm){
+      console.log(searchTerm);
     const urlString ="https://api.themoviedb.org/3/search/movie?api_key=e7e24a5efae7dd5aecf08d05791f3312&query=" +searchTerm;
     $.ajax({
         url:urlString,
@@ -20,6 +25,11 @@ class App extends Component {
 
             const results = searchResults.results;
             var movieRows = [];
+
+
+            results.sort(function(b,a){
+                return parseInt(a.vote_average)  - parseInt(b.vote_average);
+            });
 
             results.forEach((movie) => {
                 movie.poster_src = "https://image.tmdb.org/t/p/w185"+ movie.poster_path;
@@ -52,7 +62,11 @@ class App extends Component {
             </tr>
           </tbody>
         </table>
-          <input type="text" onChange={this.searchChangeHandler.bind(this)} placeholder="enter search term"/>
+          <input type="text" onChange={this.searchChangeHandler.bind(this)} placeholder="Sök film"/>
+          <button onClick={this.onClick.bind(this)}>Sort</button>
+          <div>
+
+          </div>
           {this.state.rows}
 
 
--- src/MovieRow.js
+++ src/MovieRow.js
@@ -2,15 +2,15 @@ import React from 'react'
 
 class MovieRow extends React.Component{
     render(){
-        return <table key={this.props.movie.id}>
+        return <table className="table" key={this.props.movie.id}>
             <tbody>
             <tr>
-                <td>
-                    <img width="100" alt="poster" src={this.props.movie.poster_src}/>
+                <td width="150">
+                    <img width="150" alt="poster" src={this.props.movie.poster_src}/>
                 </td>
-                <td>
-                    {this.props.movie.title}
-                    <p>{this.props.movie.overview}</p>
+                <td className="center">
+                    <h2>{this.props.movie.title}</h2>
+                    <p className="movie-desc">{this.props.movie.overview}</p>
                 </td>
             </tr>
             </tbody>
